f7c2866cfb49c318e847ced70fdd0939e862e73f
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-06-18 23:40:34 +0530

    changes

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -9,4 +9,5 @@ class AppColors {
   static const Color blue = Colors.blue;
   static const Color red = Colors.red;
   static const Color green = Colors.green;
+  static const Color black = Colors.black;
 }
--- lib/src/auth/screens/login_screen.dart
+++ lib/src/auth/screens/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:desktop_application/constants/colors.dart';
 import 'package:desktop_application/helpers/navigation.dart';
 import 'package:desktop_application/helpers/responsive.dart';
+import 'package:desktop_application/src/auth/screens/signup_screen.dart';
 import 'package:desktop_application/src/home/screens/home_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -34,7 +36,7 @@ class LoginScreen extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 3 * AppUI.dh, bottom: 8 * AppUI.dh),
+              padding: EdgeInsets.symmetric(vertical: 3 * AppUI.dh),
               child: TextFormField(
                 textInputAction: TextInputAction.next,
                 style: TextStyle(fontSize: 12 * AppUI.sp),
@@ -45,9 +47,21 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Forgot password?',
+                style: TextStyle(
+                  fontSize: 12 * AppUI.sp,
+                  color: AppColors.active,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Container(
               height: 6 * AppUI.dh,
               width: double.infinity,
+              margin: EdgeInsets.only(top: 6 * AppUI.dh, bottom: 2 * AppUI.dh),
               child: ElevatedButton(
                 onPressed: () {
                   Navigation.navigatorKey.currentState!.push(
@@ -67,6 +81,34 @@ class LoginScreen extends StatelessWidget {
                     fontWeight: FontWeight.w700,
                   ),
                 ),
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 12 * AppUI.sp,
+                  color: AppColors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+                children: [
+                  const TextSpan(text: 'Don\'t have an account?  '),
+                  TextSpan(
+                    text: 'Sign up',
+                    style: const TextStyle(
+                      color: AppColors.active,
+                      decoration: TextDecoration.underline,
+                      decorationColor: AppColors.active,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Navigation.navigatorKey.currentState!.push(
+                          MaterialPageRoute(
+                            builder: (context) => const SignUpScreen(),
+                          ),
+                        );
+                      },
+                  ),
+                ],
               ),
             ),
           ],
